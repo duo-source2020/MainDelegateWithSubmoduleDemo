55128f07833690ef401bde3ac8534eb114d367ba
Christina.huang <Christina.huang@halliburton.com>
2020-08-29 12:25:19 -0500

    Add files

---
+++ BusinessLogics/MathOperations.cs
@@ -0,0 +1,20 @@
+﻿namespace DelegateDemo
+{
+	public class MathOperations
+	{
+		public static int Add(int a, int b)
+		{
+			return a + b;
+		}
+
+		public static int Multiply(int a1, int a2)
+		{
+			return a1 * a2;
+		}
+
+		public static int Union(int a, int b)
+		{
+			return a * 10 + b;
+		}
+	}
+}
---
+++ DelegateMain/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using DelegateDemo;
+using OperationPlatForm;
+
+
+namespace DelegateMain
+{
+	class Program
+	{
+		
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("please enter two integers");
+
+			var firstInt = int.Parse(Console.ReadLine());
+			var secondInt = int.Parse(Console.ReadLine());
+			//var thirdInt = int.Parse(Console.ReadLine());
+
+			//original
+			//var addOperation = new MathOperation(Add);
+
+			//c# 2.0 - inline
+			//MathOperation addOperation = delegate(int a, int b) { return a + b; };
+
+			//C# 3.0 - lambda
+			//MathOperation addOperation =  (a, b)=>( a + b );
+
+			
+			Console.WriteLine("Add result: {0}", Operation.Run(firstInt, secondInt, MathOperations.Add));
+			Console.WriteLine("Multiply result: {0}", Operation.Run(firstInt, secondInt, MathOperations.Multiply));
+			Console.WriteLine("Union result: {0}", Operation.Run(firstInt, secondInt, MathOperations.Union));
+
+			Console.ReadLine();
+		}
+
+		
+	}
+
+}
